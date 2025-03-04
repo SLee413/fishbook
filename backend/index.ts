2d484e1bee04df518d84e2b6eca945fc8d314b30
Spencer <128531779+SLee413@users.noreply.github.com>
2025-03-04 13:39:19 -0500

    added start of mongoDB integration

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,28 @@
+import { Collection, Db } from 'mongodb';
+import { getDatabase } from './clients/mongoclient';
+
 const express = require('express');
 const app = express();
 
+// Configure environment variables
+import dotenv from 'dotenv';
+dotenv.config();
 
 app.use('/', (req, res) => {
   res.send(200);
 });
+
+// Posts API
+app.route('/posts')
+  .get(async (req, res) => {
+    const database : Db = await getDatabase();
+    const postsCollection : Collection<Comment> = await database.collection("Comments");
+
+    
+    res.send('feed of posts will go here');
+  })
+  .post((req, res) => {
+    res.send('Add a post here');
+  })
 
 app.listen(5000, () => console.log('API is running on http://localhost:5000/'));
