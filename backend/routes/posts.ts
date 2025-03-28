769cc0c9dc3e4a7f11793885dc72d704c0c33c80
Spencer <128531779+SLee413@users.noreply.github.com>
2025-03-28 14:20:47 -0400

    comments route

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,7 +1,7 @@
 /**
  * @file Controls the posts API
  * @description
- * This is an ExpressJS Router that specifically handles posts
+ * This is an ExpressJS Router that specifically handles posts and comments
  * 
  * @author Spencer Lee
  * 
@@ -9,7 +9,7 @@
 
 import { Collection, Db, ObjectId } from 'mongodb';
 import { getDatabase } from '../clients/mongoclient';
-import { Post, postSchema, User, Comment } from '../schemas/index';
+import { Post, postSchema, User, Comment, commentSchema } from '../schemas/index';
 
 const express = require('express');
 const router = express.Router();
@@ -79,6 +79,64 @@ router.post('/user/:userid', async (req, res) => {
     }
 });
 
+// Get comments of a post
+router.get('/:postid/comments', async (req, res) => {
+    // TODO: pagination
+    const database : Db = await getDatabase();
+    const commentsCollection : Collection<Comment> = await database.collection("Comments");
 
+    // Ensure postid is valid
+    if (!ObjectId.isValid(req.params.postid)) return res.status(404).send("Invalid post");
+
+    let comments = await commentsCollection.find({
+        postId : new ObjectId(req.params.postid)
+    })
+        .sort({ createdAt: -1 }) // Sort in descending order by createdAt
+        .limit(10) // Limit the result to 5 documents
+        .toArray(); // Convert the result to an array
+
+    res.send(comments);
+});
+
+// Create a comment on a post
+router.post('/:postid/comments', async (req, res) => {
+    // TODO: authentication
+    const database : Db = await getDatabase();
+    const postsCollection : Collection<Post> = await database.collection("Posts");
+    const commentsCollection : Collection<Comment> = await database.collection("Comments");
+
+    // Ensure postid is valid
+    if (!ObjectId.isValid(req.params.postid)) return res.status(404).send("Invalid post");
+
+    // Ensure post exists
+    let post = await postsCollection.findOne({
+        _id : new ObjectId(req.params.postid)
+    });
+    if (post == null) return res.status(404).send("Invalid post");
+
+    let newComment : Comment = {
+        postId : post._id,
+        datePosted : new Date(),
+        authorId : new ObjectId("67e09f9a6bb442d479dc5d87")
+        // TODO: add authorId from authenticated user
+    }
+
+    // Transfer properties from body to comment
+    for (let property in req.body) {
+        if (!newComment[property]) {
+            newComment[property] = req.body[property];
+        }
+    }
+    let result = commentSchema.safeParse(newComment);
+
+    // Validate user
+    if (!result.success) return res.status(400).send('Invalid comment');
+
+    let commentData : Comment = result.data;
+        
+    commentsCollection.insertOne(commentData);
+
+    res.status(201).send("Created");
+});
 
 export const postsRouter = router;
